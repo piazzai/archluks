890b92defe90bdc05bfb39d40cae2e11219c66ea
piazzai <42124135+piazzai@users.noreply.github.com>
2025-01-24 15:44:12 +0000

    Prompt for sudo credentials

--- setup.sh
+++ setup.sh
@@ -2,12 +2,12 @@
 set -e
 
 # prompt sudo user credentials
-read -rp "username: " username
-if [ "$username" == '' ] ; then echo "error: username required" && exit 1 ; fi
-read -rsp "password: " userpw
-if [ "$userpw" == '' ] ; then echo "error: password required" && exit 1 ; fi
-read -rsp "repeat password: " check
-if [ "$check" != "$userpw" ] ; then echo "error: passwords do not match" && exit 1 ; fi
+read -rp 'Set a name for the sudo user: ' username
+if [ "$username" == '' ] ; then echo 'Username required. Aborting.' && exit 1 ; fi
+read -rsp 'Set a password for the sudo user: ' password
+if [ "$password" == '' ] ; then echo 'Password required. Aborting.' && exit 1 ; fi
+read -rsp 'Repeat the password: ' check
+if [ "$check" != "$password" ] ; then echo 'Passwords do not match. Aborting.' && exit 1 ; fi
 
 # install bootloader
 bootctl install
@@ -16,9 +16,9 @@ bootctl install
 uuid=$(blkid /dev/nvme0n1p2 | sed 's/.* UUID="\([^\"]\).*/\1/')
 printf '%s\n' 'title Arch Linux' \
   'linux /vmlinuz-linux' \
-  "initrd /intel-ucode.img" \
+  'initrd /intel-ucode.img' \
   'initrd /initramfs-linux.img' \
-  "options cryptdevice=UUID=$(blkid /dev/nvme0n1p2 | sed 's/.* UUID="\([^\"]\).*/\1/'):vg root=/dev/mapper/vg-root rw" \
+  "options cryptdevice=UUID=$uuid:vg root=/dev/mapper/vg-root rw" \
   > /boot/loader/entries/arch.conf
 
 # add modules and hooks to initial ramdisk
@@ -41,5 +41,5 @@ printf '%s\n' '127.0.0.1 localhost' '::1 localhost' >> /etc/hosts
 
 # create sudo user
 useradd -m -G wheel "$username"
-echo "$username:$userpw" | chpasswd
+echo "$username:$password" | chpasswd
 sed -i 's/^# \(%wheel ALL=(ALL:ALL) ALL\)/\1/' /etc/sudoers
