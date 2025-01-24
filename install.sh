38343917508a245c0c0c4cde4ec8005f8ad150dd
piazzai <42124135+piazzai@users.noreply.github.com>
2025-01-24 15:14:21 +0000

    Simplify scripts

--- install.sh
+++ install.sh
@@ -1,49 +1,24 @@
 #!/bin/bash
 set -e
 
-read -rp "What is the name of your device? [/dev/nvme0n1] " device
-device=${device:-'/dev/nvme0n1'}
-
-read -rp "What is the name of your cpu make? [intel] " cpu
-cpu=${cpu:-'intel'}
-
-read -rp "Set EFI partition size: [1G] " efi
-efi=${efi:-'1G'}
-
-read -rp "Set swap partition size: [4G] " swap
-swap=${swap:-'4G'}
-
-read -rp "What is the name of your WiFi network? " wifiname
-if [ "$wifiname" == '' ] ; then echo "Error: WiFi name required." && exit 1 ; fi
-
-read -rp "What is the password of your WiFi network? " wifipw
-if [ "$wifipw" == '' ] ; then echo "Error: WiFi password required." && exit 1 ; fi
-
-printf '%s\n' "Device: $device" "CPU make: $cpu" "EFI size: $efi " "Swap size: $swap" "WiFi network: $wifiname"
-read -rp "Proceed (y/N)? " choice
-if [[ "$choice" != [Yy] && "$choice" != [Yy][Ee][Ss] ]] ; then echo "Aborted." && exit 1 ; fi
-
-# connect to wifi network 
-iwctl --passphrase "$wifipw" station wlan0 connect "$wifiname"
-
 # set clock
 timedatectl set-ntp true
 
 # wipe current signature and (optionally) securely erase all data
-wipefs -a "$device"
-read -rp "Do you want to securely erase all data on the device (y/N)? " choice
-if [[ "$choice" == [Yy] || "$choice" == [Yy][Ee][Ss] ]] ; then pv /dev/urandom -o "$device" ; fi
+wipefs -a /dev/nvme0n1
+read -rp 'Do you want to securely erase data on the device (y/N)? ' choice
+if [[ "$choice" == [Yy] || "$choice" == [Yy][Ee][Ss] ]] ; then pv /dev/urandom -o /dev/nvme0n1 ; fi
 
 # create new partitions
-printf '%s\n' 'label: gpt' ",$efi,U," ',,L,' | sfdisk "$device"
-mkfs.vfat -F 32 "${device}p1"
-cryptsetup luksFormat "${device}p2"
-cryptsetup luksOpen "${device}p2" crypt
+printf '%s\n' 'label: gpt' ',500M,U,' ',,L,' | sfdisk /dev/nvme0n1
+mkfs.vfat -F 32 /dev/nvme0n1p1
+cryptsetup luksFormat /dev/nvme0n1p2
+cryptsetup luksOpen /dev/nvme0n1p2 crypt
 
 # create logical volumes
 pvcreate /dev/mapper/crypt
 vgcreate vg /dev/mapper/crypt
-lvcreate -L "$swap" vg -n swap
+lvcreate -L 64G vg -n swap
 lvcreate -l 100%FREE vg -n root
 mkswap /dev/mapper/vg-swap
 mkfs.ext4 /dev/mapper/vg-root
@@ -51,12 +26,12 @@ mkfs.ext4 /dev/mapper/vg-root
 # mount volumes
 mount /dev/mapper/vg-root /mnt
 mkdir /mnt/boot
-mount "${device}p1" /mnt/boot
+mount /dev/nvme0n1p1 /mnt/boot
 swapon /dev/mapper/vg-swap
 
 # bootstrap arch installation
 reflector --latest 10 --sort rate --fastest 5 --save /etc/pacman.d/mirrorlist
-pacstrap -K /mnt base "$cpu-ucode" linux linux-firmware lvm2 networkmanager sudo
+pacstrap -K /mnt base intel-ucode linux linux-firmware lvm2 networkmanager sudo
 
 # generate fstab
 genfstab -U /mnt >> /mnt/etc/fstab
--- setup.sh
+++ setup.sh
@@ -1,41 +1,24 @@
 #!/bin/bash
 set -e
 
-read -rp "What is the name of your device again? [/dev/nvme0n1] " device
-device=${device:-'/dev/nvme0n1'}
-
-read -rp "What is the name of your CPU make again? [intel] " cpu
-cpu=${cpu:-'intel'}
-
-read -rp "Set timezone: [UTC] " tz
-tz=${tz:-'UTC'}
-
-read -rp "Set locale: [en_US] " locale
-locale=${locale:-'en_US'}
-
-read -rp "Set hostname: [arch] " hostname
-hostname=${hostname:-'arch'}
-
-read -rp "Set username: " username
-if [ "$username" == '' ] ; then echo "Error: username required." && exit 1 ; fi
-
-read -rp "Set password: " userpw
-if [ "$userpw" == '' ] ; then echo "Error: password required." && exit 1 ; fi
-
-printf '%s\n' "Device: $device" "CPU make: $cpu" "Timezone: $tz"  "Locale: $locale" "Hostname: $hostname" "Username: $username"
-read -rp "Proceed (y/N)? " choice
-if [[ "$choice" != [Yy] && "$choice" != [Yy][Ee][Ss] ]] ; then echo "Aborted." && exit 1 ; fi
+# prompt sudo user credentials
+read -rp "username: " username
+if [ "$username" == '' ] ; then echo "error: username required" && exit 1 ; fi
+read -rsp "password: " userpw
+if [ "$userpw" == '' ] ; then echo "error: password required" && exit 1 ; fi
+read -rsp "repeat password: " check
+if [ "$check" != "$userpw" ] ; then echo "error: passwords do not match" && exit 1 ; fi
 
 # install bootloader
 bootctl install
 
 # create boot entry
-uuid=$(blkid "${device}p2" | sed 's/.* UUID="\([^\"]\).*/\1/')
 printf '%s\n' 'title Arch Linux' \
   'linux /vmlinuz-linux' \
-  "initrd /$cpu-ucode.img" \
+  "initrd /intel-ucode.img" \
   'initrd /initramfs-linux.img' \
-  "options cryptdevice=UUID=$uuid:vg root=/dev/mapper/vg-root rw" > /boot/loader/entries/arch.conf
+  "options cryptdevice=UUID=$(blkid /dev/nvme0n1p2 | sed 's/.* UUID="\([^\"]\).*/\1/'):vg root=/dev/mapper/vg-root rw" \
+  > /boot/loader/entries/arch.conf
 
 # add modules and hooks to initial ramdisk
 sed -i 's/^MODULES=()/MODULES=(nvme)/' /etc/mkinitcpio.conf
@@ -43,19 +26,21 @@ sed -i 's/^\(HOOKS.*\)block\(.*\)/\1block encrypt lvm2\2/' /etc/mkinitcpio.conf
 mkinitcpio -p linux
 
 # set hardware clock
-timedatectl set-timezone "$tz"
+timedatectl set-timezone UTC
 hwclock --systohc
 
 # generate locale
-sed -i "s/^#\($locale.UTF-8\)/\1/" /etc/locale.gen
+sed -i "s/^#\(en_US.UTF-8\)/\1/" /etc/locale.gen
 locale-gen
-printf '%s' "LANG=$locale.UTF-8" > /etc/locale.conf
+printf '%s' 'LANG=en_US.UTF-8' > /etc/locale.conf
 
 # set hostname and add hosts entries
-printf '%s' "$hostname" > /etc/hostname
+printf '%s' 'arch' > /etc/hostname
 printf '%s\n' '127.0.0.1 localhost' '::1 localhost' >> /etc/hosts
 
 # create sudo user
 useradd -m -G wheel "$username"
 echo "$username:$userpw" | chpasswd
 sed -i 's/^# \(%wheel ALL=(ALL:ALL) ALL\)/\1/' /etc/sudoers
+
+exit
