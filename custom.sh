cf9c4891ccf7fffdf39f334a3fa1d2a7998029e9
piazzai <42124135+piazzai@users.noreply.github.com>
2025-05-16 15:50:26 +0100

    Set lowercase

--- custom.sh
+++ custom.sh
@@ -148,9 +148,10 @@ R -e "install.packages('renv', lib = Sys.getenv('R_LIBS_USER'), repos = c(CRAN =
 
 # install vscodium and extensions
 yay -S vscodium-bin
-codium --install-extension James-Yu.latex-workshop
+codium --install-extension james-yu.latex-workshop
 codium --install-extension ms-python.python
-codium --install-extension REditorSupport.r
+codium --install-extension ms-python.debugpy
+codium --install-extension reditorsupport.r
 codium --install-extension timonwong.shellcheck
 
 # install network editor
