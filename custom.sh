7d29c88a86266cd92870eaf31433b6e3c9e682ef
piazzai <42124135+piazzai@users.noreply.github.com>
2025-11-19 23:37:15 +0000

    Install iosevka slab

--- custom.sh
+++ custom.sh
@@ -263,6 +263,7 @@ sudo pacman -S noto-fonts-emoji
 sudo pacman -S ttc-iosevka
 sudo pacman -S ttc-iosevka-aile
 sudo pacman -S ttc-iosevka-etoile
+sudo pacman -S ttc-iosevka-slab
 sudo pacman -S ttf-nerd-fonts-symbols
 sudo pacman -S ttf-nerd-fonts-symbols-mono
 
