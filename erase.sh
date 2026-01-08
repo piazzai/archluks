682e00bdaca72e11c36cef62e70a9a984f45b48c
piazzai <42124135+piazzai@users.noreply.github.com>
2026-01-08 10:22:19 +0000

    Set device variable

--- erase.sh
+++ erase.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-read -rp "Do you want to securely erase all data on /dev/nvme0n1? [y/N] " ERASE
+DEVICE=/dev/nvme0n1
+
+read -rp "Do you want to securely erase all data on $DEVICE? [y/N] " ERASE
 
 if [[ "$ERASE" == [Yy] || "$ERASE" == [Yy][Ee][Ss] ]]; then
   wipefs -a "$DEVICE"
